d57fd6f50851388c0620d2fbddd1e3939acd966b
Crhistian Villantoy <crhistian.villantoy@hotmail.com>
2024-07-30 21:36:45 -0500

    empty entry

--- app.js
+++ app.js
@@ -1,39 +1,55 @@
+const WARNING_MESSAGE = "Solo letras minúsculas y sin acentos";
+const COPY_MESSAGE = "Copiado en portapapeles";
+const COPY = "Copiar";
+
 function encrypt() {
     let text = document.getElementById("textInput").value;
     if (!validInput(text)) {
-        alert("El texto debe contener solo letras minúsculas y sin acentos.");
+        alert(WARNING_MESSAGE);
         return;
     }
-    document.getElementById("informationSection").style.display = "none";
-    document.getElementById("outputSection").style.display = "flex";
-    document.getElementById("outputText").textContent = text
-        .replace(/e/g, "enter")
-        .replace(/i/g, "imes")
-        .replace(/a/g, "ai")
-        .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
+    if (text === "") {
+        document.getElementById("informationSection").style.display = "flex";
+        document.getElementById("outputSection").style.display = "none";
+    } else {
+        document.getElementById("informationSection").style.display = "none";
+        document.getElementById("outputSection").style.display = "flex";
+        document.getElementById("outputText").textContent = text
+            .replace(/e/g, "enter")
+            .replace(/i/g, "imes")
+            .replace(/a/g, "ai")
+            .replace(/o/g, "ober")
+            .replace(/u/g, "ufat");
+    }
 }
 
 function decrypt() {
     let text = document.getElementById("textInput").value;
     if (!validInput(text)) {
-        alert("El texto debe contener solo letras minúsculas y sin acentos.");
+        alert(WARNING_MESSAGE);
         return;
     }
-    document.getElementById("outputText").textContent = text
-        .replace(/enter/g, "e")
-        .replace(/imes/g, "i")
-        .replace(/ai/g, "a")
-        .replace(/ober/g, "o")
-        .replace(/ufat/g, "u");
+    if (text === "") {
+        document.getElementById("informationSection").style.display = "flex";
+        document.getElementById("outputSection").style.display = "none";
+    } else {
+        document.getElementById("informationSection").style.display = "none";
+        document.getElementById("outputSection").style.display = "flex";
+        document.getElementById("outputText").textContent = text
+            .replace(/enter/g, "e")
+            .replace(/imes/g, "i")
+            .replace(/ai/g, "a")
+            .replace(/ober/g, "o")
+            .replace(/ufat/g, "u");
+    }
 }
 
 function copy() {
     let text = document.getElementById("outputText").textContent;
     navigator.clipboard.writeText(text).then(() => {
-        document.getElementById("buttonCopy").innerText = "Copiado en portapapeles";
+        document.getElementById("buttonCopy").innerText = COPY_MESSAGE;
         setTimeout(() => {
-            document.getElementById("buttonCopy").innerText = "Copiar";
+            document.getElementById("buttonCopy").innerText = COPY;
         }, 2000);
     });
 }
